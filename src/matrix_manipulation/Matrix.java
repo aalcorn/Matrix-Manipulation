a69144a44900ef8c198f12b8b887519e1c131b18
Adam <aalcorn@stu.jsu.edu>
2019-07-01 20:52:22 -0400

    Fixed a bug, Row Echelon form works

--- src/matrix_manipulation/Matrix.java
+++ src/matrix_manipulation/Matrix.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matrix_manipulation;
 import java.util.*;
 
@@ -12,10 +8,10 @@ import java.util.*;
  */
 public class Matrix {
     
-    private int[][] matrix =  {  {1, 0, 0},
-                                 {0, 1, 0},
-                                 {0, 0, 1}  };
-    private int[] aug = {1, 1, 1};
+    private int[][] matrix =  {  {0, 0, 0},
+                                 {6, 12, 18},
+                                 {7, 8, 9}  };
+    private int[] aug = {3, 6, 1};
     
     public Matrix() {
         
@@ -44,9 +40,11 @@ public class Matrix {
         boolean firstFound = false;
         int belowNum;
         int rowsLeft;
-        int pivot = 0;
+        int pivotLoc = 0;
         int[] tempArr;
         int tempAug;
+        int[] tempAugArr;
+        int pivot = 1;
         //find first non-zero number in the row (pivot).
         //make the pivot 1 by dividing the entire row by itself.
         //make every number blow the pivot 0 by adding or subtracting a multiple of the row from that row.
@@ -55,31 +53,35 @@ public class Matrix {
             for(int j = 0; j < matrix[i].length; j++) { //find the pivot and make it 1
                 if((matrix[i][j] != 0) && firstFound == false) {
                     firstFound = true;
-                    pivot = j;
+                    pivotLoc = j;
+                    pivot = matrix[i][j];
+                    System.out.println("Making pivot 1... " + "row " + (i + 1 ));
                     for(int m = 0; m < matrix[i].length; m++) {
-                        matrix[i][m] = matrix[i][m] / matrix[i][j];
+                        matrix[i][m] = matrix[i][m] / pivot;
                         if(m == 0) {
-                            aug[i] = aug[i] / matrix[i][j];
+                            aug[i] = aug[i] / pivot;
                         }
                         showMatrix();
-                        System.out.println("Making pivot 1... " + "row " + i );
                     }
                 }
             }
             for(int n = i + 1; n < matrix.length; n++) { //add a multiple of the pivot row to get 0s under the pivot
-                for(int l = 0; l < matrix[i].length; l++) {
-                    tempArr = Arrays.copyOf(matrix[i],matrix[i].length);
-                    for(int k : tempArr) {
-                        k *= matrix[n][pivot];
-                    }
-                    tempAug = aug[i] *= matrix[n][pivot];
-                    System.out.println("Subtracting " + matrix[n][pivot] + " times row " + i + " from row " + n);
-                    for(int g = 0; g < matrix[n].length; g++) {
-                        matrix[n][g] -= tempArr[g];
-                    }
-                    aug[n] -= tempAug;
-                    showMatrix();
-                } 
+                tempArr = Arrays.copyOf(matrix[i],matrix[i].length);
+                tempAugArr = Arrays.copyOf(aug,aug.length);
+                /*for(int k : tempArr) {
+                    k *= matrix[n][pivotLoc];
+                }*/
+                for(int k = 0; k < tempArr.length; k++) {
+                    tempArr[k] = tempArr[k] * matrix[n][pivotLoc];
+                }
+                tempAug = tempAugArr[i] *= matrix[n][pivotLoc];
+                System.out.println("Subtracting " + matrix[n][pivotLoc] + " times row " + (i + 1) + " from row " + (n + 1));
+                for(int g = 0; g < matrix[n].length; g++) {
+                    matrix[n][g] -= tempArr[g];
+                }
+                aug[n] -= tempAug;
+                showMatrix();
+                
             }
         }
         
--- src/matrix_manipulation/Matrix_Manipulation.java
+++ src/matrix_manipulation/Matrix_Manipulation.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matrix_manipulation;
 
 /**
@@ -11,11 +7,8 @@ package matrix_manipulation;
  */
 public class Matrix_Manipulation {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
         Matrix m = new Matrix();
         m.rowEchelonForm();
         
